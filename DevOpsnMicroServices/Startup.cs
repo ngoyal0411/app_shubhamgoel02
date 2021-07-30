784a3c59423de141748ab2c08f09f22dcf80c92e
Shubham Goel <shubham.goel02@nagarro.com>
2021-07-30 11:54:53 +0530

    added default home

--- DevOpsnMicroServices/Startup.cs
+++ DevOpsnMicroServices/Startup.cs
@@ -26,6 +26,7 @@ namespace DevOpsnMicroServices
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvcCore().AddRazorViewEngine();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +45,7 @@ namespace DevOpsnMicroServices
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
         }
     }
